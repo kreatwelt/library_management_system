2a6c943aa61fb7675729b61c15539534ad7cf0b1
Oluwatimilehin John Ikumawoyi <71076052+kreatwelt@users.noreply.github.com>
2025-04-04 19:05:53 +0500

    Add files via upload

---
+++ dashboard/user_dashboard.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+if ($_SESSION["role"] !== "user") {
+    header("Location: ../auth/login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>User Dashboard</title>
+    <link rel="stylesheet" href="../static/style.css">
+</head>
+<body class="user-dashboard">
+    <div class="container">
+        <h2>User Dashboard</h2>
+        <a href="../user/borrow_book.php">Borrow a Book</a><br>
+        <a href="../user/return_book.php">Return a Book</a><br>
+        <a href="../auth/logout.php">Logout</a>
+    </div>
+</body>
+</html>
