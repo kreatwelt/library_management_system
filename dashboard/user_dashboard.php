c6e0263cc8ace3f3034c6ddff430ccc9c03e48b3
Oluwatimilehin John Ikumawoyi <71076052+kreatwelt@users.noreply.github.com>
2025-04-08 18:52:34 +0500

    Update user_dashboard.php

--- dashboard/user_dashboard.php
+++ dashboard/user_dashboard.php
@@ -17,6 +17,7 @@ if ($_SESSION["role"] !== "user") {
         <h2>User Dashboard</h2>
         <a href="../user/borrow_book.php">Borrow a Book</a><br>
         <a href="../user/return_book.php">Return a Book</a><br>
+        <a href="../user/search_book.php">Search for a Book</a><br>
         <a href="../auth/logout.php">Logout</a>
     </div>
 </body>
